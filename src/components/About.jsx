4fed1c186d09709409fddaa6df8250a840b12512
alihuzaifa <ha4587323@gmail.com>
2023-09-24 23:19:39 +0500

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,17 @@ const About = () => {
   return (
     <MotionLayout>
       <Stack pt={15} id="About" width={{ xs: "100%", md: "60%" }}>
-        <Typography variant="h4" fontWeight="bold">About me</Typography>
+        <Typography variant="h4" fontWeight="bold">
+          About me
+        </Typography>
         <Typography pt={2} color={Grey} fontSize={useResponsiveFont ? 16 : 14}>
-          A passionate Full Stack Software Developer, Ali Huzaifa brings expertise in crafting dynamic Web and Mobile applications using JavaScript, React.js, Node.js, React Native, and a suite of other cutting-edge libraries and frameworks. With a strong foundation in code architecture and a flair for innovation, Ali transforms ideas into functional and visually appealing digital solutions. Let's collaborate and bring your projects to life with skill and enthusiasm!
+          A passionate Full Stack Software Developer, Ali Huzaifa brings
+          expertise in crafting dynamic Web and Mobile applications using
+          JavaScript, React.js, Node.js, React Native, and a suite of other
+          cutting-edge libraries and frameworks. With a strong foundation in
+          code architecture and a flair for innovation, Ali transforms ideas
+          into functional and visually appealing digital solutions. Let's
+          collaborate and bring your projects to life with skill and enthusiasm!
         </Typography>
       </Stack>
     </MotionLayout>
